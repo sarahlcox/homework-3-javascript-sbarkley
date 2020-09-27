8227d736d8d8cf8d72be84194a1cd6566add5d2d
sarahlcox <sarahl.cox.22@gmail.com>
2020-09-27 11:22:17 -0500

    added pseudocode

--- Develop/script.js
+++ Develop/script.js
@@ -1,25 +1,48 @@
 // Defining Variables
-var symbol= "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+var symbol= ['!','?','@'];//"!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 var upper= "ABCDEFGHIJKLLMNOPQRSTUVWXYZ";
-var lower= "abcdefghijklmnopqrstuvwxyz";
-var numeric= "1234567890";
+var lower= ['a','b','c'];//"abcdefghijklmnopqrstuvwxyz";
+var numeric= [1,2,3,4];//"1234567890";
 
 // User Prompts
-var symbolGen = confirm ("Do you want your password to contain special characters?");
+var symbolGen;
+var userLength;
+
 var upperGen = confirm ("Do you want your password to contain uppercase letters?");
 var lowerGen = confirm ("Do you want your password to contain lowercase letters?");
 var numericGen = confirm ("Do you want your password to contain numbers?");
+var userschoice=[];
+var finalpw;
 
 // define variable for generated password
 //generate  return fx and it return a password
 function generatePassword(){
+  //ask for length, prompt returns out a string, you will need to convert this into a number
+  //parseInt();
 
   //ask user questions
-  //decide how the possible data is stored (userschoice)
-  //based on user length get random vals from the userschoice
-  //return the final password
+  symbolGen = confirm ("Do you want your password to contain special characters?");
+  //if symbolGen is true, what do we do?
+    //store it in userschoice.
+     //decide how the possible data is stored (userschoice: [],{}=>
+      // [] b/c we have a .length to determine how much is in the userschoice bc it will vary)
 
-  return"";
+
+  //after all the series of questions are answered, what do we do?
+    //generate a random password using userschoice (need to know how to call random, forloops)
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+    //Math.floor(Math.random() * (max - min) + min);
+    //based on user length (parseInt) get random vals from the userschoice
+    //return the final password
+    for(var i=0;i<userLength;i++){
+    var index=(Math.floor(Math.random() * ((userschoice.length-1) - 0) + 0);
+
+    console.log(userschoice[index]);
+    finalpw+=userschoice[index];
+    //finalpw=finalpw+userschoice[index]
+    }
+
+  return finalpw;
 
 }
 
@@ -32,21 +55,21 @@ function generatePassword(){
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//1. display fx
-function mult1(x,y){
-  console.log(x*y)
-}
+// //1. display fx
+// function mult1(x,y){
+//   console.log(x*y)
+// }
 
-mult1(2,3)  //6
-mult1(321321,42112);
+// mult1(2,3)  //6
+// mult1(321321,42112);
 
-//2. return fx toss the data to do an action
-function mult2(x,y){
-  return x*y;
-}
-var result= mult2(3,4);
-console.log(result);
-//catch the data a: store it into a var or b: console.log
+// //2. return fx toss the data to do an action
+// function mult2(x,y){
+//   return x*y;
+// }
+// var result= mult2(3,4);
+// console.log(result);
+// //catch the data a: store it into a var or b: console.log
 
 
 // Write password to the #password input
