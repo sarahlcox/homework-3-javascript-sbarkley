74e0d956826c67438dada561adab9e74916f8340
sarahlcox <sarahl.cox.22@gmail.com>
2020-09-27 22:53:45 -0500

    there was an error in the script.js document resulting in a console log error, created a new js.js document and am not seeing the issue anymore

---
+++ Develop/js.js
@@ -0,0 +1,34 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+// Create arrays for user choice
+var symbol = ["!","?","@","#","$","%","&","+","-",".",":","=","^","~"];
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numeric = ["1","2","3","4","5","6","7","8","9","0"];
+
+console.log(symbol.length);
+console.log(upper.length);
+console.log(lower.length);
+console.log(numeric.length);
+
+// User Prompt variables
+var symbolGen;
+var upperGen;
+var lowerGen;
+var numericGen;
+// var userLength;
+// var userschoice=[];
+// var finalpw;
