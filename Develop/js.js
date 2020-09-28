b972b7e8a1c706227bbafd1f4b696ac37c840bce
sarahlcox <sarahl.cox.22@gmail.com>
2020-09-28 00:01:02 -0500

    having issues with this assignment, submitted what I had and will revisit tomorrow

--- Develop/js.js
+++ Develop/js.js
@@ -1,5 +1,5 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+document.querySelector("#generate").addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
@@ -12,17 +12,11 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
 // Create arrays for user choice
 var symbol = ["!","?","@","#","$","%","&","+","-",".",":","=","^","~"];
 var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numeric = ["1","2","3","4","5","6","7","8","9","0"];
-
-console.log(symbol.length);
-console.log(upper.length);
-console.log(lower.length);
-console.log(numeric.length);
 
 // User Prompt variables
 var symbolGen;
@@ -32,3 +26,86 @@ var numericGen;
 // var userLength;
 // var userschoice=[];
 // var finalpw;
+
+// Function to generate password, user selects how long they would like password to be
+function generatePassword(){
+  var userLength = pa(prompt("How many characters would you like your password? Choose between 8 and 128"));
+  // First if statement for user validation 
+if (userLength <= 8 || userLength >= 128) {
+    alert("You must choose between 8 and 128");
+    var userLength = (prompt("How long would you like your password to be? Choose 8-128 characters."));
+}
+
+// ask user questions
+var symbolGen = confirm ("Do you want your password to contain special characters?");
+var lowerGen = confirm ("Do you want your password to contain lowercase letters?");
+var upperGen = confirm ("Do you want your password to contain uppercase letters?");
+var numericGen = confirm ("Do you want your password to contain numbers?");
+};
+
+ // If user doesnt choose one selection need to alert to pick at least one
+ while(symbolGen === false && upperGen === false && lowerGen === false && numericGen === false) {
+  alert("You must make at least one selection.");
+  var symbolGen = confirm("Click OK to confirm if you would like to include special characters");
+  var lowerGen = confirm("Click OK to confirm if you would like to include lowercase characters");
+  var upperGen = confirm("Click OK to confirm if you would like to include uppercase characters");
+  var numericGen = confirm("Click OK to confirm if you would like to include numeric characters");
+} 
+  //if symbolGen is true, what do we do?
+    //store it in userschoice.
+     //decide how the possible data is stored (userschoice: [],{}=>
+      // [] b/c we have a .length to determine how much is in the userschoice bc it will vary)
+
+//     for(var i=0;i<userLength;i++){
+//     var index=(Math.floor(Math.random() * ((userschoice.length-1) - 0) + 0);
+//generate return function and it returns a password
+var userschoice = []
+      
+  if (symbolGen) {
+    userschoice = userschoice.concat(symbol)
+  }
+
+  if (lowerGen) {
+    userschoice = userschoice.concat(lower)
+  }
+    
+  if (upperGen) {
+    userschoice = userschoice.concat(upper)
+  }
+
+  if (numericGen) {
+    userschoice = userschoice.concat(numeric)
+  }
+
+    console.log(userschoice)
+
+    // Empty string to be filled based on for loop selecting random characters from the array
+    var finalpw = ""
+
+    //after all the series of questions are answered, what do we do?
+    //generate a random password using userschoice (need to know how to call random, forloops)
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+    //Math.floor(Math.random() * (max - min) + min);
+    //based on user length (parseInt) get random vals from the userschoice
+    //return the final password
+    for (var i = 0; i < userLength; i++) {
+      finalpw = finalpw + userschoice[Math.floor(Math.random() * userschoice.length)];
+      console.log(finalpw)
+    }
+    return finalpw;
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+//     console.log(userschoice[index]);
+//     finalpw+=userschoice[index];
+//     //finalpw=finalpw+userschoice[index]
+//     }
+
+//   return finalpw;
